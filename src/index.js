47ddaf617167ff71643eec68860534b1e7d32d4e
kostyayakimovich <yakimovichkostya@gmail.com>
2020-03-08 11:39:34 +0300

    feat: solution task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+const checkIfArrayIsValid = array =>
+    !Array.isArray(array) ||
+    !Boolean(array.length) ||
+    (array.length === 1 && array[0] === 0);
+exports.min = function min(array) {
+    if (checkIfArrayIsValid(array)) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    array.sort((a, b) => a - b);
 
-exports.max = function max (array) {
-  return 0;
-}
+    return array[0];
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (checkIfArrayIsValid(array)) return 0;
+
+    array.sort((a, b) => a - b);
+
+    return array[array.length - 1];
+};
+
+exports.avg = function avg(array) {
+    if (checkIfArrayIsValid(array)) return 0;
+
+    const res = array.reduce(
+        (previousValue, currentItem) => previousValue + currentItem,
+        0
+    );
+
+    return res / array.length;
+};
